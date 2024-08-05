ea1af60b253faade3fae398764c93cc28dcc2a91
RinnHH <ElizavetaHitrova2012@email.ru>
2024-08-05 18:10:11 +0300

    1 урок Python

---
+++ main.py
@@ -0,0 +1,19 @@
+# print("Hello world!")
+# print("Hitrova")
+# print("Мой возраст 12 лет")
+# print("Hello, world!")
+# print("Лиза")
+# print((2+2/4)*4)
+# print(((3+2)*4)/((2*2)+3/4))
+# # print(((3-(-3**2))/(9*3))*4)
+# print(((3-(-3**2))/(9*3))*4)
+a = 'Мой возраст: '
+b = 25
+c = ' лет.'
+print(a+str(b)+c)
+
+a = '10'
+b = 2.0
+print(int(a)+int(b))
+
+print(b+str(t)+a2+v)
